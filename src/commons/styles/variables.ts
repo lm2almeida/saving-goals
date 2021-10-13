597e7f3d641d6ee0384931b37862fbf9cbcf2bed
Lucas Almeida <lm2.almeida@gmail.com>
2021-10-13 01:35:39 -0300

    chore(styles): add variables * colors-blue-gray-300 * spacing-xsmall

--- src/commons/styles/variables.ts
+++ src/commons/styles/variables.ts
@@ -8,6 +8,7 @@ const variables = css`
     --colors-blue-gray-10: #f4f8fa;
     --colors-blue-gray-50: #e9eef2;
     --colors-blue-gray-100: #cbd5dc;
+    --colors-blue-gray-300: #8a9ca9;
     --colors-blue-gray-400: #708797;
     --colors-blue-gray-600: #4d6475;
     --colors-blue-gray-900: #1e2a32;
@@ -15,6 +16,7 @@ const variables = css`
     --colors-brand-secondary: #0079ff;
     --colors-neutral-white: #ffffff;
 
+    --spacing-xsmall: 8px;
     --spacing-small: 12px;
     --spacing-medium: 16px;
     --spacing-large: 20px;
