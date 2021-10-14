df851bf8d6e3b6a070b5da7507ff55cf837297f1
Lucas Almeida <lm2.almeida@gmail.com>
2021-10-14 09:18:08 -0300

    test(SavingForm): update tests

--- src/components/SavingForm/SavingForm.test.tsx
+++ src/components/SavingForm/SavingForm.test.tsx
@@ -1,16 +1,38 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import SavingForm from '.';
 
 describe('components/SavingForm', () => {
-  it('should render correctly', () => {
+  beforeAll(() => {
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(new Date(2021, 9, 1));
+  });
+
+  it('should render correct results when user interacts', () => {
     render(<SavingForm />);
 
-    expect(screen.getByTestId('saving-goal')).toBeInTheDocument();
-    expect(screen.getByTestId('input-amount')).toBeInTheDocument();
-    expect(screen.getByTestId('input-reachDate')).toBeInTheDocument();
-    expect(
-      screen.getByRole('button', { name: /confirm/i })
-    ).toBeInTheDocument();
+    const incrementButton = screen.getByLabelText(/increment month/i);
+    const decrementButton = screen.getByLabelText(/decrement month/i);
+    const reachGoalInput = screen.getByLabelText(/reach goal by/i);
+
+    userEvent.type(screen.getByLabelText(/total amount/i), '10000');
+    userEvent.click(incrementButton);
+    userEvent.click(incrementButton);
+
+    reachGoalInput.focus();
+    userEvent.keyboard('[ArrowRight]');
+
+    expect(screen.getByText(/january 2022/i)).toBeInTheDocument();
+
+    userEvent.click(decrementButton);
+
+    reachGoalInput.focus();
+    userEvent.keyboard('[ArrowLeft]');
+
+    expect(screen.getByText(/november 2021/i)).toBeInTheDocument();
+    expect(screen.getByText(/\$5,000/i)).toBeInTheDocument();
+    expect(screen.getByText(/2 monthly deposits/i)).toBeInTheDocument();
+    expect(screen.getByText(/\$10,000/i)).toBeInTheDocument();
   });
 });
